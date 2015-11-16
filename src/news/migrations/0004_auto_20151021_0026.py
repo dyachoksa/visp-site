5ab8a65ca50d1afcab7a54c607a875e186cdd749
Sergey Dyachok <sergey@sdyachok.com.ua>
2015-11-16 23:46:04 +0200

    Added migration

---
+++ src/news/migrations/0004_auto_20151021_0026.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import ckeditor_uploader.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('news', '0003_auto_20150720_2343'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='news',
+            name='content',
+            field=ckeditor_uploader.fields.RichTextUploadingField(verbose_name='content'),
+        ),
+    ]
